27ab721d847738898f8b5d7eeae429d95181c2b7
Ross <rosskugler@gmail.com>
2025-02-01 15:26:54 -0800

    Working on player weapons

---
+++ BulletHellGame/BulletHellGame/Data/DataTransferObjects/WeaponData.cs
@@ -0,0 +1,14 @@
+﻿namespace BulletHellGame.Data.DataTransferObjects
+{
+    public class WeaponData
+    {
+        public string SpriteName { get; set; }
+        public BulletData bulletData {  get; set; }
+        public float FireRate {  get; set; }
+        public float TimeSinceLastShot { get; set; }
+
+        public List<Vector2> FireDirections { get; set; }
+
+        public WeaponData() { }
+    }
+}
---
+++ BulletHellGame/BulletHellGame/Factories/WeaponFactory.cs
@@ -0,0 +1,20 @@
+﻿using BulletHellGame.Data.DataTransferObjects;
+using BulletHellGame.Entities;
+using BulletHellGame.Managers;
+
+namespace BulletHellGame.Factories
+{
+    public class WeaponFactory
+    {
+        public WeaponFactory() { }
+
+        public Entity CreateWeapon(WeaponData weaponData)
+        {
+            SpriteData spriteData = TextureManager.Instance.GetSpriteData(weaponData.SpriteName);
+            Entity weapon = new Entity();
+            weapon.AddComponent(new SpriteComponent(spriteData));
+            //weapon.AddComponent(new
+            return weapon;
+        }
+    }
+}
