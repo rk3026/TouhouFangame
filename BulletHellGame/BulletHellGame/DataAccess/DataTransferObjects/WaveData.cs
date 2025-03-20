a023f5d1c1bd02a382933591bd26984bbd22935c
Huy Ky <huyky26@gmail.com>
2025-03-20 15:49:19 -0700

    Changed wavedata.cs

--- BulletHellGame/BulletHellGame/DataAccess/DataTransferObjects/WaveData.cs
+++ BulletHellGame/BulletHellGame/DataAccess/DataTransferObjects/WaveData.cs
@@ -3,6 +3,7 @@
     public class WaveData
     {
         public float StartTime { get; set; } // When this wave starts in seconds
+        public float Duration { get; set; } // How long this wave lasts in seconds
         public List<EnemySpawnData> Enemies { get; set; } = new List<EnemySpawnData>();
     }
 }
