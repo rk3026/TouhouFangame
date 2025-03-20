2d06e6a3be13f9e0cadda1686103923ec179e969
majdaltouzach <majdaltoufic88@tutanota.com>
2025-03-20 15:48:18 -0700

    Created LevelWaveLoader.cs

---
+++ BulletHellGame/BulletHellGame/DataAccess/DataLoaders/LevelWaveLoader.cs
@@ -0,0 +1,12 @@
+namespace BulletHellGame.DataAccess.DataLoaders;
+
+/**
+ * Implementing leve1.json with 5 waves. Reference DataTransferObjects (LevelData, WaveData)
+ * Wave 1, 2: Standard Grunt fights
+ * Wave 3: Mid-level boss
+ * Wave 4: 
+ */
+public class LevelWaveLoader
+{
+    
+}
