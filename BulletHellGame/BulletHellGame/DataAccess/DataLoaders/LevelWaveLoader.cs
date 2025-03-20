1a2b772d299e5a3089f1f9b13e8b8a01a9c86e38
majdaltouzach <majdaltoufic88@tutanota.com>
2025-03-20 15:49:48 -0700

    LevelWaveLoader.cs Csharp inline comments

--- BulletHellGame/BulletHellGame/DataAccess/DataLoaders/LevelWaveLoader.cs
+++ BulletHellGame/BulletHellGame/DataAccess/DataLoaders/LevelWaveLoader.cs
@@ -4,7 +4,8 @@ namespace BulletHellGame.DataAccess.DataLoaders;
  * Implementing leve1.json with 5 waves. Reference DataTransferObjects (LevelData, WaveData)
  * Wave 1, 2: Standard Grunt fights
  * Wave 3: Mid-level boss
- * Wave 4: 
+ * Wave 4: Grunt fights with additional complexity
+ * Wave 5: Final boss which can spawn grunts in its path
  */
 public class LevelWaveLoader
 {
