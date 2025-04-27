f773641dfd387f2fe12d582e0b025821af077875
Huy Ky <huyky26@gmail.com>
2025-04-27 03:31:37 -0700

    add loot table json, cook some levels

--- BulletHellGame/BulletHellGame/DataAccess/DataLoaders/LootTableLoader.cs
+++ BulletHellGame/BulletHellGame/DataAccess/DataLoaders/LootTableLoader.cs
@@ -1,21 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using BulletHellGame.DataAccess.DataTransferObjects;
 using BulletHellGame.Logic.Entities;
-using BulletHellGame.Logic.Utilities.EntityDataGenerator;
-using BulletHellGame.Logic.Utilities.EntityDataGenerator.EntityDataGenerators;
 
 namespace BulletHellGame.DataAccess.DataLoaders
 {
     public static class LootTableLoader
     {
+        const string LOOT_JSON_PATH = "./Data/Levels/loots.json";
+
+        private static Random _random = new Random();
+
         public static List<CollectibleData> GetLoot(string lootTableId)
         {
-            return CollectibleDataGenerator.GenerateRandomLoot();
+            var lootTableJson = JsonUtility.LoadJson(LOOT_JSON_PATH)[lootTableId];
+
+            var lootTable = new List<(CollectibleData item, float weight)>();
+
+            foreach (var lootJson in lootTableJson)
+            {
+                string sprite = lootJson["sprite"].Value<string>();
+                CollectibleType type = Enum.Parse<CollectibleType>(lootJson["type"].Value<string>());
+                int quantity = lootJson["quantity"].Value<int>();
+                float probability = lootJson["probability"].Value<float>();
+
+                lootTable.Add((new CollectibleData { SpriteName = sprite, Effects = { { type, quantity } } }, probability));
+            }
+
+            var selectedLoot = new List<CollectibleData>();
+
+            foreach (var (item, weight) in lootTable)
+            {
+                if (_random.NextDouble() < weight) // Compare against weight (probability)
+                {
+                    selectedLoot.Add(item);
+                }
+            }
+
+            return selectedLoot;
         }
     }
 }
