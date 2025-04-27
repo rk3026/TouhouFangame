31dfecc37bfe553a515deb940418c61fabf2a8e2
Dylan Gyori <gyoridylan@gmail.com>
2025-04-26 22:46:39 -0700

    cleaned up level data loader

--- BulletHellGame/BulletHellGame/DataAccess/DataLoaders/LevelDataLoader.cs
+++ BulletHellGame/BulletHellGame/DataAccess/DataLoaders/LevelDataLoader.cs
@@ -29,7 +29,7 @@ namespace BulletHellGame.DataAccess.DataLoaders
         public static List<int> GetLevelIds()
         {
             var levelsJson = JsonUtility.LoadJson(LEVEL_JSON_PATH);
-            
+
             List<int> levelsIds = new List<int>();
             foreach (var level in levelsJson)
             {
@@ -48,7 +48,7 @@ namespace BulletHellGame.DataAccess.DataLoaders
         {
             var levelJson = JsonUtility.LoadJson(LEVEL_JSON_PATH)[levelId];
 
-            
+
 
             List<WaveData> waves = new List<WaveData>();
             foreach (string waveId in levelJson["waveIds"])
@@ -65,62 +65,5 @@ namespace BulletHellGame.DataAccess.DataLoaders
 
             return level;
         }
-
-        public static LevelData LoadLevel(string levelName)
-        {
-            return null;
-        }
-
-        private static readonly string LevelDirectory = "Data/Levels";
-
-       
-
-        
-
-        private static LevelData ConvertDtoToLevelData(LevelDataDto dto)
-        {
-            return new LevelData
-            {
-                LevelId = dto.level_id,
-                LevelName = dto.level_name,
-                Background = dto.background,
-                Music = dto.music,
-                Waves = dto.waves.Select(w => new WaveData
-                {
-                    StartTime = w.spawn_time,
-                    Formation = w.formation,
-                    //Enemies = w.enemies.Select(e => EnemyDataLoader.LoadEnemyData(e.id)).ToList(),
-                }).ToList(),
-            };
-        }
-    }
-
-    public class LevelDataDto
-    {
-        public int level_id { get; set; }
-        public string level_name { get; set; }
-        public string background { get; set; }
-        public string music { get; set; }
-        public List<WaveDto> waves { get; set; }
-    }
-
-    public class WaveDto
-    {
-        public float spawn_time { get; set; }
-        public string formation { get; set; }
-        public List<EnemyDto> enemies { get; set; }
-    }
-
-    public class EnemyDto
-    {
-        public string id { get; set; }
-        public PositionDto spawn_position { get; set; }
-        public int health { get; set; }
-    }
-
-    public class PositionDto
-    {
-        public float x { get; set; }
-        public float y { get; set; }
     }
 }
